fedcce918258d98e3ed6486141c80197869f554b
tonimota <toni.elton1993@gmail.com>
2019-03-28 22:10:42 -0300

    [REFEC] Method create list

--- src/script.js
+++ src/script.js
@@ -1,31 +1,24 @@
 let url = "https://private-21e8de-rafaellucio.apiary-mock.com/users";
+let data = "";
 
 function init() {
   fetch(url).then(r => r.json())
-  .then(data => 
-    // console.log(data)
-    data.forEach(item => {
-      createElement(item)
-    })
-  )
-  .catch(e => 
+  .then(data => {
+      createElement(data)
+  })
+  .catch(e => {
     console.log("Deu ruim")
-  );
+  });
 }
 
 function createElement(data) {
   let boxList = document.querySelector('.box-list-itens');
-  let item = document.createElement('li');
-  let id = document.createElement('span')
-  let name = document.createElement('span');
-  item.appendChild(id);
-  item.appendChild(name);
-  item.classList.add('item');
-  id.classList.add('id');
-  id.innerText = data.cpf;
-  name.classList.add('name');
-  name.innerText = data.name;
-  boxList.appendChild(item);
+  data.forEach(item => {
+    let item_list = document.createElement('li');
+    let el = `<span class="cpf">${item.cpf}</span><span class="name">${item.name}</span><span class="email">${item.email}</span>`;
+    item_list.innerHTML = el
+    boxList.appendChild(item_list);
+  })
 }
 
 window.addEventListener('load', function() {
