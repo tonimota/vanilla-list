7cb25bd07d60d888a067c07924ca2b0ccbb28d5c
Toni <toni.elton1993@gmail.com>
2019-03-29 16:02:23 -0300

    [FIX] Resolve conflicts and fix list

--- src/script.js
+++ src/script.js
@@ -1,31 +1,46 @@
-let url = "https://private-21e8de-rafaellucio.apiary-mock.com/users";
+let url = "https://jsonplaceholder.typicode.com/users";
+let data = "";
 
 function init() {
   fetch(url).then(r => r.json())
-  .then(data => 
-    // console.log(data)
-    data.forEach(item => {
-      createElement(item)
-    })
-  )
-  .catch(e => 
-    console.log("Deu ruim")
-  );
+  .then(data => {
+    createElement(data);
+  })
+  .catch(e => {
+    console.log("Deu ruim");
+  });
 }
 
 function createElement(data) {
   let boxList = document.querySelector('.box-list-itens');
-  let item = document.createElement('li');
-  let id = document.createElement('span')
-  let name = document.createElement('span');
-  item.appendChild(id);
-  item.appendChild(name);
-  item.classList.add('item');
-  id.classList.add('id');
-  id.innerText = data.cpf;
-  name.classList.add('name');
-  name.innerText = data.name;
-  boxList.appendChild(item);
+  let title = `
+    <strong class="title column">Name</strong>
+    <strong class="title column">Website</strong>
+    <strong class="title column">Email</strong>
+    <strong class="title column">Phone</strong>`;
+  let title_item = document.createElement('li');
+  title_item.classList.add('title_item');
+  title_item.innerHTML = title;
+  boxList.appendChild(title_item);
+  data.forEach((item, i) => {
+    let item_list = document.createElement('li');
+    item_list.classList.add('item')
+    let el = `
+    <p class="column">
+      <span class="name">${item.name}</span>
+    </p>
+    <p class="column">
+      <span class="website"><a href="${item.website}">${item.website}</a></span>
+    </p>
+    <p class="column">
+      <span class="email">${item.email}</span>
+    </p>
+    <p class="column">
+      <span class="phone">${item.phone}</span>
+    </p>`;
+    item_list.innerHTML = el;
+    boxList.appendChild(item_list);
+  })
 }
 
 window.addEventListener('load', function() {
